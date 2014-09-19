0c62ac41377693519154f5f2652eff45270bb06e
Gordon Bateson <gorodnbateson@gmail.com>
2014-09-19 13:47:46 +0900

    mod/hotpot fix 'version value is missing from version.php'

--- version.php
+++ version.php
@@ -29,13 +29,16 @@
 // prevent direct access to this script
 defined('MOODLE_INTERNAL') || die();
 
-$plugin = new stdClass();
+if (floatval($GLOBALS['CFG']->release) <= 2.6) {
+    $plugin = new stdClass();
+}
+
 $plugin->cron      = 0;
 $plugin->component = 'mod_hotpot';
 $plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;      // Moodle 2.0
-$plugin->release   = '2014.09.19 (30)';
-$plugin->version   = 2014091930;
+$plugin->release   = '2014.09.19 (31)';
+$plugin->version   = 2014091931;
 
 if (floatval($GLOBALS['CFG']->release) <= 2.6) {
     $module = clone($plugin);
