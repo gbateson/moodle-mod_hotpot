a1d6ab919c5d7ba14f25e844cdc8a9147136a525
Gordon Bateson <gorodnbateson@gmail.com>
2014-09-19 09:59:59 +0900

    mod/hotpot fix 'Missing version value in version.php' in Moodle 2.4 - 2.5

--- version.php
+++ version.php
@@ -34,8 +34,8 @@ $plugin->cron      = 0;
 $plugin->component = 'mod_hotpot';
 $plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;      // Moodle 2.0
-$plugin->release   = '2014.09.19 (28)';
-$plugin->version   = 2014091928;
+$plugin->release   = '2014.09.19 (29)';
+$plugin->version   = 2014091929;
 
 if (floatval($CFG->version) <= 2.6) {
     $module = clone($plugin);
