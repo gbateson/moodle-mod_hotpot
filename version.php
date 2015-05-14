9b56fad3ad2a3505295d5f55aa7e8f2717b78b75
Gordon Bateson <gorodnbateson@gmail.com>
2015-05-14 10:07:34 +0900

    mod/hotpot improve Moodle version detection using compare_version

--- version.php
+++ version.php
@@ -34,9 +34,9 @@ if (empty($CFG)) {
 }
 
 if (isset($CFG->release)) {
-    $moodle_26 = (floatval($CFG->release) <= 2.6);
+    $moodle_26 = version_compare($CFG->release, '2.6.99', '<=');
 } else if (isset($CFG->yui3version)) {
-    $moodle_26 = (floatval($CFG->yui3version) <= 3.13);
+    $moodle_26 = version_compare($CFG->yui3version, '3.13.99', '<=');
 } else {
     $moodle_26 = false;
 }
@@ -49,8 +49,8 @@ $plugin->cron      = 0;
 $plugin->component = 'mod_hotpot';
 $plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;      // Moodle 2.0
-$plugin->release   = '2015.05.13 (67)';
-$plugin->version   = 2015051367;
+$plugin->release   = '2015.05.14 (68)';
+$plugin->version   = 2015051468;
 
 if ($moodle_26) {
     $module = clone($plugin);
