8eef30dd8e09ddb209f954506184be80060ed410
Gordon Bateson <gordonbateson@gmail.com>
2015-10-29 10:46:16 +0900

    mod/hotpot standardize white spce in JS template files for JCloze and JQuiz

--- version.php
+++ version.php
@@ -49,8 +49,8 @@ $plugin->cron      = 0;
 $plugin->component = 'mod_hotpot';
 $plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;      // Moodle 2.0
-$plugin->release   = '2015-10-28 (79)';
-$plugin->version   = 2015102879;
+$plugin->release   = '2015-10-29 (80)';
+$plugin->version   = 2015102980;
 
 if ($moodle_26) {
     $module = clone($plugin);
