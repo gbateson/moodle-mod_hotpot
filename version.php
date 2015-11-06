78ba6346e4f5f5960f4f84689fe57bdf3f7c70b6
Gordon Bateson <gordonbateson@gmail.com>
2015-11-06 19:33:42 +0900

    mod/hotpot fix SQL syntax error in upgrade script

--- version.php
+++ version.php
@@ -49,8 +49,8 @@ $plugin->cron      = 0;
 $plugin->component = 'mod_hotpot';
 $plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;      // Moodle 2.0
-$plugin->release   = '2015-11-05 (83)';
-$plugin->version   = 2015110583;
+$plugin->release   = '2015-11-06 (84)';
+$plugin->version   = 2015110684;
 
 if ($moodle_26) {
     $module = clone($plugin);
