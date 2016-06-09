01d18ccf9d968ff9a11fc5b59dfb1ab04617d223
Gordon Bateson <gordonbateson@gmail.com>
2016-06-09 13:52:21 +0900

    mod_hotpot use $CFG->branch to detect Moodle's major version in version.php

--- version.php
+++ version.php
@@ -33,15 +33,7 @@ if (empty($CFG)) {
     global $CFG;
 }
 
-if (isset($CFG->release)) {
-    $moodle_26 = version_compare($CFG->release, '2.6.99', '<=');
-} else if (isset($CFG->yui3version)) {
-    $moodle_26 = version_compare($CFG->yui3version, '3.13.99', '<=');
-} else {
-    $moodle_26 = false;
-}
-
-if ($moodle_26) {
+if (empty($CFG->branch) || $CFG->branch <= 26) {
     $plugin = new stdClass();
 }
 
@@ -49,9 +41,9 @@ $plugin->cron      = 0;
 $plugin->component = 'mod_hotpot';
 $plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
 $plugin->requires  = 2010112400;      // Moodle 2.0
-$plugin->release   = '2016-06-01 (92)';
-$plugin->version   = 2016060192;
+$plugin->release   = '2016-06-09 (93)';
+$plugin->version   = 2016060993;
 
-if ($moodle_26) {
+if (empty($CFG->branch) || $CFG->branch <= 26) {
     $module = clone($plugin);
 }
