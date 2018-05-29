15eab58c64d1cd8c2eb3dd890e45a8791476b3f4
Gordon Bateson <gordonbateson@gmail.com>
2018-05-30 08:39:06 +0900

    mod/hotpot add output format descriptor strings for Rottmeier DropDown and JGloss

--- lang/en/hotpot.php
+++ lang/en/hotpot.php
@@ -423,8 +423,10 @@ $string['outputformat_help'] = 'The output format specifies how the content will
 The output formats that are available depend on the type of the source file. Some types of source file have just one output format, while other types of source file have several output formats.
 
 The "best" setting will display the content using the optimal output format for the student\'s browser.';
+$string['outputformat_hp_6_jcloze_html_dropdown'] = 'DropDown from html';
 $string['outputformat_hp_6_jcloze_html_findit_a'] = 'FindIt (a) from html';
 $string['outputformat_hp_6_jcloze_html_findit_b'] = 'FindIt (b) from html';
+$string['outputformat_hp_6_jcloze_html_jgloss'] = 'JGloss from html';
 $string['outputformat_hp_6_jcloze_html'] = 'JCloze (v6) from html';
 $string['outputformat_hp_6_jcloze_xml_anctscan'] = 'ANCT-Scan from HP6 JCloze xml';
 $string['outputformat_hp_6_jcloze_xml_dropdown'] = 'DropDown from HP6 JCloze xml';
