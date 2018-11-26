6bc0fc730c95e3ddf32a526f01feece26a436a7b
Anupama Dharmajan <anupamadharmajan@catalyst-au.net>
2018-11-27 10:03:14 +1100

    Incorporated Privacy API
    Added missing functions in privacy provider

--- attempt/qedoc/lang/en/hotpotattempt_qedoc.php
+++ attempt/qedoc/lang/en/hotpotattempt_qedoc.php
@@ -1,2 +1,3 @@
 <?php
 $string['pluginname'] = 'Qedoc output formats';
+$string['privacy:metadata'] = 'The Qedoc output formats module does not store any personal data.';
--- lang/en/hotpot.php
+++ lang/en/hotpot.php
@@ -613,3 +613,19 @@ $string['weighting'] = 'Weighting';
 $string['wrong'] = 'Wrong';
 $string['zeroduration'] = 'No duration';
 $string['zeroscore'] = 'Zero score';
+
+$string['privacy'] = 'Privacy of results';
+$string['privacy:metadata:hotpot_attempts'] = 'Information about the user attempts at HotPot activities';
+$string['privacy:metadata:hotpot_attempts:hotpotid'] = 'The ID of the attempts at HotPot activities';
+$string['privacy:metadata:hotpot_attempts:userid'] = 'The ID of the user attempts at HotPot activities';
+$string['privacy:metadata:hotpot_attempts:starttime'] = 'The starttime indicating when the attempts was started by the user';
+$string['privacy:metadata:hotpot_attempts:endtime'] = 'The endtime indicating when the attempts was ended by the user';
+$string['privacy:metadata:hotpot_attempts:score'] = 'The score indicating the score received for the attempts by the user';
+$string['privacy:metadata:hotpot_attempts:penalties'] = 'The penalties indicating the penalties received for the attempts by the user';
+$string['privacy:metadata:hotpot_attempts:attempt'] = 'The attempt indicating the number of attempt by the user';
+$string['privacy:metadata:hotpot_attempts:timestart'] = 'The timestart indicating when the attempts was started by the user';
+$string['privacy:metadata:hotpot_attempts:timefinish'] = 'The timefinish indicating when the attempts was ended by the user';
+$string['privacy:metadata:hotpot_attempts:status'] = 'The status indicating status of the attempts by the user';
+$string['privacy:metadata:hotpot_attempts:clickreportid'] = 'The clickreportid indicating report id of the click by the user';
+$string['privacy:metadata:hotpot_attempts:timemodified'] = 'The timemodified indicating when the HotPot attempts was modified by the user';
+
